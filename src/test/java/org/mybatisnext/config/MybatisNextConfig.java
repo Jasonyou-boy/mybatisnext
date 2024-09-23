9c343ec7a2d5f1f52e867dff4cff43d9c89837f7
Jasonyou <2926593225@qq.com>
2024-09-24 01:49:45 +0800

    test:  TestSqlHandler.java

--- src/test/java/org/mybatisnext/config/MybatisNextConfig.java
+++ src/test/java/org/mybatisnext/config/MybatisNextConfig.java
@@ -1,28 +1,25 @@
 package org.mybatisnext.config;
 
 import com.google.common.io.Resources;
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatisnext.HandlerContext;
-import org.mybatisnext.handler.SqlHandler;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
 @Configuration
 public class MybatisNextConfig {
 
-    void register(){
-        HandlerContext.registerHandler("dept",sql -> {
-            return "dept=1";
+    void register() {
+        //示例:根据当前用户Id生成条件
+        int nowUserId = 1;
+        HandlerContext.registerHandler("user", sql -> {
+            return "user_id=" + nowUserId;
         });
+
     }
 
     @Bean
@@ -35,8 +32,5 @@ public class MybatisNextConfig {
             throw new RuntimeException(e);
         }
     }
-    @Bean
-    DataSource dataSource(SqlSessionFactory sqlSessionFactory ){
-        return sqlSessionFactory.getConfiguration().getEnvironment().getDataSource();
-    }
+
 }
